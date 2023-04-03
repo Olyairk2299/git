10c1b4653cd0c51deebc84533d58aa0a1191f499
OlyaTalancina <Talankina.irk@gmail.com>
2023-04-03 15:55:13 +0800

    edit readme.md

--- git.py
+++ git.py
@@ -1,4 +1,4 @@
-git commit -m Initial commit
-edit readme.md #Синхронизурует с Git
-pip install -r requirements.txt. #Вссе версии библиотек
-add requirements.txt # Синхронизация 2 в Git
+# git commit -m Initial commit
+# edit readme.md #Синхронизурует с Git
+# pip install -r requirements.txt. #Вссе версии библиотек
+# add requirements.txt # Синхронизация 2 в Git
