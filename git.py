88d36ac91dfa709a8ccc09bac9c67b5e61c56b44
OlyaTalancina <Talankina.irk@gmail.com>
2023-04-03 14:26:16 +0800

    add requirements.txt

--- git.py
+++ git.py
@@ -1,2 +1,4 @@
 git commit -m Initial commit
-edit readme.md
+edit readme.md #Синхронизурует с Git
+pip install -r requirements.txt. #Вссе версии библиотек
+add requirements.txt # Синхронизация 2 в Git
